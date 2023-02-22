064ae2145a1a7c5f0da20b5bcdcf082ddd3e680a
Edwin Cardenas <eacardenase@unal.edu.co>
2023-02-22 17:06:43 -0500

    Adding Tab Bar encapsulated into MainViewController

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+let appColor: UIColor = .systemTeal
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
@@ -15,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let loginViewController = LoginViewController()
     let onboardingContainerViewController = OnboardingContainerViewController()
     let dummyViewController = DummyViewController()
+    let mainViewController = MainViewController()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
@@ -25,8 +28,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         onboardingContainerViewController.delegate = self
         dummyViewController.delegate = self
         
-        window?.rootViewController = loginViewController
+        window?.rootViewController = mainViewController
 //        window?.rootViewController = onboardingContainerViewController
+        
+//        mainViewController.selectedIndex = 2
         
         return true
     }
---
+++ Bankey/Utils/UIViewController+Utils.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewController+Utils.swift
+//  Bankey
+//
+//  Created by Edwin Cardenas on 2/22/23.
+//
+
+import UIKit
+
+extension UIViewController {
+    func setStatusBar() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground() // hides Navigation Bar line
+        navBarAppearance.backgroundColor = appColor
+        
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+    }
+    
+    func setTabBarImage(imageName: String, title: String) {
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+}
