50ccfb2dab2c8bf10f90f668ffc285caccd92125
Edwin Cardenas <eacardenase@unal.edu.co>
2023-02-26 18:32:53 -0500

    Adding testBreakDollarsIntoCents, testDollarsFormatted and testZeroDollarsFormatted unit tests

---
+++ BankeyUnitTests/CurrencyFormatterTests.swift
@@ -0,0 +1,40 @@
+//
+//  CurrencyFormatterTests.swift
+//  BankeyUnitTests
+//
+//  Created by Edwin Cardenas on 2/26/23.
+//
+
+import Foundation
+import XCTest
+
+@testable import Bankey
+
+class Test: XCTestCase {
+    var formatter: CurrencyFormatter!
+    
+    override func setUp() {
+        super.setUp()
+        
+        formatter = CurrencyFormatter()
+    }
+    
+    func testBreakDollarsIntoCents() throws {
+        let result = formatter.breakIntoDollarsAndCents(929466.23)
+        
+        XCTAssertEqual(result.0, "929,466")
+        XCTAssertEqual(result.1, "23")
+    }
+    
+    func testDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(929466.23)
+        
+        XCTAssertEqual(result, "$929,466.23")
+    }
+    
+    func testZeroDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(0.00)
+        
+        XCTAssertEqual(result, "$0.00")
+    }
+}
