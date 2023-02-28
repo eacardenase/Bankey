1ae4f9e3019691bbbecbd58160973df0f793eb22
Edwin Cardenas <eacardenase@unal.edu.co>
2023-02-28 11:37:44 -0500

    Adding ShakeyBellView animation

--- Bankey/Components/ShakeyBellView.swift
+++ Bankey/Components/ShakeyBellView.swift
@@ -61,8 +61,60 @@ extension ShakeyBellView {
 
 extension ShakeyBellView {
     @objc func imageViewTapped(_ recognizer: UITapGestureRecognizer) {
-        // animation
+        shakeWith(duration: 1.0, angle: .pi / 8, yOffset: 0.0)
+    }
+    
+    private func shakeWith(duration: Double, angle: CGFloat, yOffset: CGFloat) {
+        let numberOfFrames: Double = 6
+        let frameDuration = Double(1 / numberOfFrames)
         
-        print("Shaking!!!")
+        imageView.setAnchorPoint(CGPoint(x: 0.5, y: yOffset))
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0, animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform(rotationAngle: -angle)
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: frameDuration, relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform(rotationAngle: +angle)
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: frameDuration * 2, relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform(rotationAngle: -angle)
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: frameDuration * 3, relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform(rotationAngle: +angle)
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: frameDuration * 4, relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform(rotationAngle: -angle)
+            }
+            
+            UIView.addKeyframe(withRelativeStartTime: frameDuration * 5, relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform.identity
+            }
+        })
+    }
+}
+
+extension UIView {
+    func setAnchorPoint(_ point: CGPoint) {
+        var newPoint = CGPoint(x: bounds.size.width * point.x, y: bounds.size.height * point.y)
+        var oldPoint = CGPoint(x: bounds.size.width * layer.anchorPoint.x, y: bounds.size.height * layer.anchorPoint.y)
+        
+        newPoint = newPoint.applying(transform)
+        oldPoint = oldPoint.applying(transform)
+        
+        var position = layer.position
+        
+        position.x -= oldPoint.x
+        position.x += newPoint.x
+        
+        position.y -= oldPoint.y
+        position.y += newPoint.y
+        
+        layer.position = position
+        layer.anchorPoint = point
     }
 }
